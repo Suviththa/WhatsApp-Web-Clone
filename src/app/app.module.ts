a7b766115a346042be215f5d097a575fe738e305
Suviththa <suviyoga2@gmail.com>
2022-06-19 13:35:49 +0530

    Completed Main page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { PickerModule } from '@ctrl/ngx-emoji-mart';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { ChatComponent } from './components/chat/chat.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
     SidebarComponent,
-    ChatComponent
+    ChatComponent,
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    PickerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
